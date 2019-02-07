7cfe7dde6894e445ce8feb817da288e346c4bd2d
sushil.kuma <sushil.kuma@LP-5CD82812CG.mshome.net>
2019-02-07 14:10:52 +0530

    removing eureka support

--- src/main/java/com/food/order/registration/app/Application.java
+++ src/main/java/com/food/order/registration/app/Application.java
@@ -1,20 +1,10 @@
 package com.food.order.registration.app;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 
-@EnableDiscoveryClient
 @SpringBootApplication
 @ComponentScan(basePackages = { "com.food.order.registration" })
 public class Application {
@@ -40,15 +30,3 @@ public class Application {
 	}*/
 }
 
-@RestController
-class ServiceInstanceRestController {
-
-    @Autowired
-    private DiscoveryClient discoveryClient;
-
-    @RequestMapping("/service-instances/{applicationName}")
-    public List<ServiceInstance> serviceInstancesByApplicationName(
-            @PathVariable String applicationName) {
-        return this.discoveryClient.getInstances(applicationName);
-    }
-}
